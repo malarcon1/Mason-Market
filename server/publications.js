30b82cf3a92640ff37cc5399fd2bffac60bb5286
ksyedabu1 <36109203+ksyedabu1@users.noreply.github.com>
2018-04-12 22:02:29 -0400

    Published specifically Books collection

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import { publishPagination } from 'meteor/kurounin:pagination';
 import {Books} from "../lib/collections";
+
 publishPagination(Books);
 
-
+Meteor.publish('Books', function publishBooks(){
+  return Books.find();
+});
